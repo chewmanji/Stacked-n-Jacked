ee3bf24a0259d1b766ae0a2d1bf209d826c0f086
Kosmalski Wojciech <jawmen999@gmail.com>
2024-08-31 15:29:30 +0200

    feat: add endpoint - get sets of given exercise

--- Backend/GymPlannerAPI/src/crud/exercise.py
+++ Backend/GymPlannerAPI/src/crud/exercise.py
@@ -3,6 +3,9 @@ from typing import Type
 from sqlalchemy.orm import Session
 
 from src.models.exercise import Exercise as ExerciseDB
+from src.models.workout import Workout as WorkoutDB
+from src.models.workout_exercise import WorkoutExercise as WorkoutExerciseDB
+from src.models.set import Set as SetDB
 
 
 def get_exercises(db: Session, skip: int = 0, limit: int = 50) -> list[Type[ExerciseDB]]:
@@ -11,3 +14,11 @@ def get_exercises(db: Session, skip: int = 0, limit: int = 50) -> list[Type[Exer
 
 def get_exercise_by_id(db: Session, exercise_id: int) -> ExerciseDB | None:
     return db.query(ExerciseDB).get(exercise_id)
+
+
+def get_exercise_sets_by_user_id(db, exercise_id, user_id) -> list[SetDB]:
+    return (db.query(SetDB)
+            .join(SetDB.workout_exercise)
+            .join(WorkoutExerciseDB.workout)
+            .where(WorkoutDB.user_id == user_id, WorkoutExerciseDB.exercise_id == exercise_id)
+            .all())
--- Backend/GymPlannerAPI/src/routers/exercise.py
+++ Backend/GymPlannerAPI/src/routers/exercise.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import Session
 
 import src.crud.exercise as exercise_service
 from src.schemas.exercise import Exercise
-from src.core.dependencies import get_db
+from src.schemas.set import Set
+from src.schemas.user import User
+from src.core.dependencies import get_db, get_current_user
 
 router = APIRouter(prefix="/exercises", tags=["Exercise"])
 
@@ -22,3 +24,14 @@ async def get_exercise(exercise_id: Annotated[int, Path(title="Exercise ID to ge
     if exercise is None:
         raise HTTPException(status_code=404, detail="Exercise not found")
     return exercise
+
+
+@router.get("/{exercise_id}/sets", response_model=list[Set])
+async def get_exercise_sets(current_user: Annotated[User, Depends(get_current_user)],
+                            exercise_id: Annotated[int, Path(title="Exercise ID whose sets to get", gt=0)],
+                            db: Session = Depends(get_db)):
+    exercise = exercise_service.get_exercise_by_id(db, exercise_id)
+    if exercise is None:
+        raise HTTPException(status_code=404, detail="Exercise not found")
+
+    return exercise_service.get_exercise_sets_by_user_id(db, exercise_id, current_user.id)
